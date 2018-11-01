325550c8ac7d52ce0ed1f298480c5ca5df0a17cf
Matthew Hsu <matthew.hsu@samsara.com>
2018-11-01 15:40:59 -0700

    Basic sudoku support

--- client/src/app.js
+++ client/src/app.js
@@ -2,6 +2,38 @@ import React from 'react';
 import { connectGraphQL, mutate } from 'thunder-react';
 import { GraphiQLWithFetcher } from './graphiql';
 
+window.puzzleToArray = (puzzle) => {
+  let rows = puzzle.split("\n");
+  return rows.map((r) => {
+    let cols = r.split("|");
+    return cols.map((c)=> {
+      if (c === ".") {
+        return null;
+      }
+      return parseInt(c, 10);
+    });
+  });
+}
+
+window.arrayToPuzzle = (arr) => {
+  let output = "";
+  arr.forEach((r, i) => {
+    r.forEach((c, j) => {
+      output += (c == null) ? "." : '' + c;
+      if (j < r.length - 1)
+        output += '|';
+    });
+    if (i < arr.length - 1) {
+      output += "\n";
+    }
+  });
+  return output
+}
+
+let testPuzzle = "6|.|.|.|.|3|.|.|9\n.|.|4|.|1|.|6|.|7\n1|.|.|.|.|.|.|.|.\n.|.|.|4|.|9|.|5|.\n.|2|.|.|.|.|.|7|.\n9|.|7|.|.|.|8|.|4\n.|9|.|.|8|.|.|.|.\n.|8|.|3|.|2|.|9|.\n.|.|.|.|.|.|5|2|.";
+console.log(testPuzzle == window.arrayToPuzzle(window.puzzleToArray(testPuzzle)));
+
+
 const Editor = React.createClass({
   getInitialState() {
     return {text: ''};
